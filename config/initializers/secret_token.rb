5e57973fa383782deb24594510d7af1f5abc3501
bhennes2 <bhennes2@gmail.com>
2012-04-04 16:33:32 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tracker::Application.config.secret_token = '6ab1b03bf0aa6cc90c858959c212bd336309adad154542ab676d3b15c798d84c25e490c5cb858f874c82a842ec18f5df0fb4edd64eb5bf312a7ee1586b66b8a1'
